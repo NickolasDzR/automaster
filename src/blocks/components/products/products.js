ae979d3ade9a9e15a896de7a260b5cd922f689f1
NickolasDzR <nikolyae@mail.ru>
2020-07-21 15:48:24 +0300

    products block

--- src/blocks/components/products/products.js
+++ src/blocks/components/products/products.js
@@ -3,8 +3,35 @@ import Swiper from '../../../../node_modules/swiper/swiper-bundle';
 const productSlider = document.querySelector('.products__slider');
 
 const productSwiper = new Swiper(productSlider, {
-    slidesPerView: 4,
+    slidesPerView: 3,
     slidesPerColumn: 4,
     slidesPerColumnFill: 'row',
-    allowTouchMove: false,
+    breakpoints: {
+        992: {
+            slidesPerView: 4,
+            slidesPerColumn: 3,
+        },
+        768: {
+            slidesPerView: 3,
+            slidesPerColumn: 4,
+            allowTouchMove: false,
+        },
+        576: {
+            slidesPerView: 2,
+            slidesPerColumn: 3,
+            slidesPerGroup: 6,
+        },
+        320: {
+            slidesPerView: 1,
+            slidesPerColumn: 3,
+            slidesPerGroup: 1,
+            pagination: {
+                el: '.products__pagination',
+                clickable: true,
+                bulletClass: 'products__pagination-bullet',
+                bulletActiveClass: 'products__pagination-bullet_active',
+              },
+            allowTouchMove: true,
+        }
+    }
 })
